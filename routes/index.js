68b203d5d35cae4ddf7ef23822422357a49797a4
BadTom <ajrih.k.i.23@gmail.com>
2024-11-17 21:33:09 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+  router.get('/musson', function(req, res, next) {
+    res.send("<h1>Страница Муссона</h1>")
+    });
+    router.get('/mistral', function(req, res, next) {
+      res.send("<h1>Страница Мистраль</h1>")
+      });
+    router.get('/sundowner', function(req, res, next) {
+      res.send("<h1>Страница Сандаунера</h1>")
+      });
+    
 module.exports = router;
