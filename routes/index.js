e9d6535144d69ffa573c112996297c8a9568efa4
BadTom <ajrih.k.i.23@gmail.com>
2024-11-25 22:23:52 +0300

    tc_4_4

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
   router.get('/musson', function(req, res, next) {
     res.render( 'wind',{
       title: "Муссон",
-      picture: "project_wind1/public/images/mistral.png",
+      picture: " /images/musson.png ",
       desc: "Муссон — это ветер, который меняет своё направление два раза в год: летом он дует с моря на сушу, зимой — с суши на море."
     });
   });
@@ -13,14 +13,14 @@ var router = express.Router();
   router.get('/mistral', function(req, res, next) {
     res.render( 'wind',{
       title: "Мистраль",
-      picture: "project_wind1/public/images/mistral.png",
+      picture: " /images/mistral.png ",
       desc: "Мистраль — холодный северо-западный ветер, дующий во Франции с гор Севенны и юго-западных отрогов Альп в долину Роны и на средиземноморское побережье. Наблюдается во все времена года, но чаще зимой."
     });
   });
   router.get('/sundowner', function(req, res, next) {
     res.render( 'wind',{
       title: "Сандаунер",
-      picture: "project_wind1/public/images/sundowner.png",
+      picture: "/images/sundowner.png",
       desc: "Сандаунер — это северный морской ветер в Калифорнии вдоль южного тихоокеанского склона гор Санта-Инес, в населённых пунктах вдоль побережья Гавиота и Санта-Барбара в направлении округа Вентура, но не включая его."
     });
   });
