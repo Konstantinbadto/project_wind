58b322a29292e074d882d82a027f5a01cfdfb4ce
BadTom <ajrih.k.i.23@gmail.com>
2025-01-18 16:14:33 +0300

    tc_7_2

--- app1.js
+++ app1.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const wind = mongoose.model('Wind', { name: String }) ;
-const winds = new wind({ name: 'Бриз' });
-winds.save().then(() => console.log('имеется'));
+var schema = mongoose.Schema({ name: String })
+schema.methods.veter = function(){
+console.log(this.name + " подул")
+}
+const wind = mongoose.model('Wind', schema);
+const winds = new wind({ name: 'Перун' });
+winds.save().then(() => winds.veter());
