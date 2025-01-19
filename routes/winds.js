64bfd718f0c814b8f15cab4fc4a1e867329d8eb5
BadTom <ajrih.k.i.23@gmail.com>
2025-01-19 18:42:42 +0300

    tc_10_8

--- routes/winds.js
+++ routes/winds.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var Wind = require('../models/wind').Wind;
+var express = require('express');
+var router = express.Router();
+var Wind = require('../models/wind').Wind;
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('Новый маршрутизатор, для маршрутов, начинающихся с winds');
 });  
 
-router.get("/:nick", async function (req, res, next) {
+router.get("/:nick",checkAuth, async function (req, res, next) {
     var winds = await Wind.find({ nick: req.params.nick });
     console.log(winds)
     if (!winds.length) return next(new Error("Нет такого ветра"))
