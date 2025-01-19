eddd49aa3fe55f057e0d50eb2d882cbdbfadbc36
BadTom <ajrih.k.i.23@gmail.com>
2025-01-19 18:32:38 +0300

    tc_10_7

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user').User;
 
+/* POST logout. */
+router.post('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.locals.user = null;
+  res.redirect('/');
+  });
+  
+
 router.get('/', function(req, res, next) {
   req.session.greeting = "Hi!!!";
   res.render('index', { title: 'Express',
@@ -34,6 +42,7 @@ router.post('/logreg', async function(req, res, next) {
   res.redirect('/');
   } else {
   //res.send("<h1>Пользователь найден</h1>");
+
 var foundUser = users[0];
 if(foundUser.checkPassword(password)){
 req.session.user_id = foundUser._id
