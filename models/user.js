33b9ee140e9f59ae4111c6a6889cc84cd885935c
BadTom <ajrih.k.i.23@gmail.com>
2025-01-19 17:46:36 +0300

    tc_10_4

--- models/user.js
+++ models/user.js
@@ -22,28 +22,7 @@ var userSchema = new Schema({
     default: Date.now
   }
 });
-<form class="form-horizontal" action="/logreg" method="post">
-<div class="form-group">
-<label class="control-label col-sm-1">Имя</label>
-<div class="col-sm-5">
-<input class="form-control" type="text"
-placeholder="Введите имя" name="username"/>
-</div>
-</div>
-<div class="form-group">
-<label class="control-label col-sm-1">Пароль</label>
-<div class="col-sm-5">
-<input class="form-control" type="password"
-placeholder="Введите пароль" name="password"/>
-</div>
-</div>
-<div class="form-group">
-<div class="col-sm-offset-1 col-sm-5">
-<input class="btn btn-primary" type="submit"
-value="Войти"/>
-</div>
-</div>
-</form>
+
 
 
 // Добавляем виртуальное поле password
@@ -61,5 +40,9 @@ userSchema.virtual("password")
 userSchema.methods.encryptPassword = function(password){
   return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
 };
+userSchema.methods.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword
+    }
+            
 
 module.exports.User = mongoose.model("User", userSchema);
